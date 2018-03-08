2baaec4a0f3516507df4dc336cde258d5fc2a39a
Edson Andrade <pereira.edson@ctis.com.br>
2018-03-08 13:54:55 -0300

    Adicionado até aula 6.8

--- src/main/java/br/com/edsonandrade/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/edsonandrade/config/AuthorizationServerConfig.java
@@ -9,7 +9,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -24,20 +25,30 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			   .withClient("admin")
 			   .secret("admin")
 			   .scopes("read", "write")
-			   .authorizedGrantTypes("password")
-			   .accessTokenValiditySeconds(1800);
+			   .authorizedGrantTypes("password", "refresh_token")
+			   .refreshTokenValiditySeconds(3600*24)
+			   .accessTokenValiditySeconds(20);
 	}
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
 			.tokenStore(tokenStore())
+			.accessTokenConverter(accessTokenConverter())
+			.reuseRefreshTokens(false)
 			.authenticationManager(authenticationManager);
 	}
 	
 	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter jtc = new JwtAccessTokenConverter();
+		jtc.setSigningKey("algaworks");
+		return jtc;
+	}
+
+	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(accessTokenConverter());
 	}
 
 }
---
+++ src/main/java/br/com/edsonandrade/token/RefreshTokenPostProcessor.java
@@ -0,0 +1,58 @@
+package br.com.edsonandrade.token;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken> {
+
+	@Override
+	public OAuth2AccessToken beforeBodyWrite(OAuth2AccessToken body, MethodParameter arg1, MediaType arg2,
+			Class<? extends HttpMessageConverter<?>> arg3, ServerHttpRequest serverRequest, ServerHttpResponse serverRespose) {
+		
+		HttpServletRequest request = ((ServletServerHttpRequest)serverRequest).getServletRequest();
+		HttpServletResponse response= ((ServletServerHttpResponse)serverRespose).getServletResponse();
+		DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) body;
+		String refresh = body.getRefreshToken().getValue();
+		adicionarRefreshAoCookie(refresh, request, response );
+		
+		removerRefreshDoBody(token);
+		
+		return body;
+	}
+
+	private void removerRefreshDoBody(DefaultOAuth2AccessToken token) {
+		token.setRefreshToken(null);
+		
+	}
+
+	@Override
+	public boolean supports(MethodParameter arg0, Class<? extends HttpMessageConverter<?>> arg1) {
+		return arg0.getMethod().getName().equals("postAccessToken");
+	}
+	
+	private void adicionarRefreshAoCookie(String refresh, HttpServletRequest request, HttpServletResponse response) {
+		Cookie refreshCookie = new Cookie("refresh", refresh);
+		refreshCookie.setHttpOnly(true);
+		refreshCookie.setSecure(false); //TODO migra para https
+		refreshCookie.setPath(request.getContextPath() + "/oauth/token");
+		refreshCookie.setMaxAge(2592000);
+		response.addCookie(refreshCookie);
+		
+		
+	}
+
+}
