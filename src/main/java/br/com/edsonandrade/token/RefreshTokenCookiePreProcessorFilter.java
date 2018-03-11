34faa29a175c43ea497a5506489dc9fe33c581d2
EDSON <EDSON>
2018-03-11 19:50:46 -0300

    Adicionado até a aula 6.10

---
+++ src/main/java/br/com/edsonandrade/token/RefreshTokenCookiePreProcessorFilter.java
@@ -0,0 +1,76 @@
+package br.com.edsonandrade.token;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.apache.catalina.util.ParameterMap;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RefreshTokenCookiePreProcessorFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		
+		if ("/sistema/oauth/token".equalsIgnoreCase(httpRequest.getRequestURI()) 
+				&& "refresh_token".equals(httpRequest.getParameter("grant_type"))
+				&& httpRequest.getCookies() != null) {
+			for (Cookie cookie : httpRequest.getCookies()) {
+				if (cookie.getName().equals("refresh")) {
+					String refreshToken = cookie.getValue();
+					httpRequest = new MyServletRequestWrapper(httpRequest, refreshToken);
+				}
+			}
+		}
+		
+		chain.doFilter(httpRequest, response);
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	static class MyServletRequestWrapper extends HttpServletRequestWrapper {
+
+		private String refreshToken;
+		
+		public MyServletRequestWrapper(HttpServletRequest request, String refreshToken) {
+			super(request);
+			this.refreshToken = refreshToken;
+		}
+		
+		@Override
+		public Map<String, String[]> getParameterMap() {
+			ParameterMap<String, String[]> map = new ParameterMap<>(getRequest().getParameterMap());
+			map.put("refresh_token", new String[] { refreshToken });
+			map.setLocked(true);
+			return map;
+		}
+		
+	}
+
+}
